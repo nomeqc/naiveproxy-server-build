b96e6c7b944b6d74917fe606d59f1f48575d4c0f
Nomeqc <nomeqc@gmail.com>
2021-07-25 22:23:02 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
+~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
+git config --global user.email nomeqc@gmail.com
+git config --global user.name Fallrainy
+git add caddy
+git commit -m "build caddy"
+git push
