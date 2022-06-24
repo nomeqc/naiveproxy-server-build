e2ad539bf33f997854730742797ccb0f5d9e58df
Fallrainy <nomeqc@gmail.com>
2022-06-25 01:29:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
 git config --global user.email nomeqc@gmail.com
 git config --global user.name Fallrainy
